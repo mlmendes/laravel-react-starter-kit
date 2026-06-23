3e6ed72d13f6e253fdaa0dd263a4110f6fbfc5e0
Lucas Mendes Martins <martinslmendes@gmail.com>
2026-06-23 10:53:44 -0300

    feat: role permissions select all

--- resources/js/pages/roles/form.tsx
+++ resources/js/pages/roles/form.tsx
@@ -1,4 +1,5 @@
 import { Form } from '@inertiajs/react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button } from '@/components/ui/button';
 import { Checkbox } from '@/components/ui/checkbox';
@@ -22,6 +23,30 @@ type Props = {
 export default function RoleForm({ permissions, role }: Props) {
     const { t } = useTranslation();
     const formRoute = role ? roles.update(role.uuid) : roles.store();
+    const [selectedPermissions, setSelectedPermissions] = useState<Set<string>>(
+        () => {
+            const initial = new Set<string>();
+            role?.permissions?.forEach((permission) =>
+                initial.add(permission.uuid),
+            );
+
+            return initial;
+        },
+    );
+
+    const togglePermission = (uuid: string, isChecked: boolean) => {
+        setSelectedPermissions((prev) => {
+            const selectedPermissions = new Set(prev);
+
+            if (isChecked) {
+                selectedPermissions.add(uuid);
+            } else {
+                selectedPermissions.delete(uuid);
+            }
+
+            return selectedPermissions;
+        });
+    };
 
     return (
         <Form
@@ -37,10 +62,10 @@ export default function RoleForm({ permissions, role }: Props) {
             {({ errors, resetAndClearErrors }) => (
                 <FieldSet>
                     <FieldGroup>
-                        <Field data-invalid={errors.name !== undefined}>
+                        <Field data-invalid={!!errors.name}>
                             <FieldLabel htmlFor="name">{t('Name')}</FieldLabel>
                             <Input
-                                aria-invalid={errors.name !== undefined}
+                                aria-invalid={!!errors.name}
                                 autoComplete="name"
                                 defaultValue={role?.name}
                                 id="name"
@@ -53,12 +78,44 @@ export default function RoleForm({ permissions, role }: Props) {
                         <FieldSet>
                             <FieldLegend>{t('Permissions')}</FieldLegend>
                             <FieldGroup>
+                                <Field orientation="horizontal">
+                                    <Checkbox
+                                        checked={permissions.every(
+                                            (permission) =>
+                                                selectedPermissions.has(
+                                                    permission.uuid,
+                                                ),
+                                        )}
+                                        id="select-all-permissions"
+                                        onCheckedChange={(checked) => {
+                                            if (checked === true) {
+                                                setSelectedPermissions(
+                                                    new Set(
+                                                        permissions.map(
+                                                            (permission) =>
+                                                                permission.uuid,
+                                                        ),
+                                                    ),
+                                                );
+                                            } else {
+                                                setSelectedPermissions(
+                                                    new Set<string>(),
+                                                );
+                                            }
+                                        }}
+                                    />
+                                    <FieldLabel htmlFor="select-all-permissions">{t('Select all')}</FieldLabel>
+                                </Field>
                                 {permissions.map((permission) => (
                                     <Field
+                                        className="ml-8"
                                         key={permission.uuid}
                                         orientation="horizontal"
                                     >
                                         <Checkbox
+                                            checked={selectedPermissions.has(
+                                                permission.uuid,
+                                            )}
                                             defaultChecked={
                                                 !!role?.permissions.find(
                                                     (rolePermission) =>
@@ -68,6 +125,12 @@ export default function RoleForm({ permissions, role }: Props) {
                                             }
                                             id={`permissions[${permission.uuid}]`}
                                             name={`permissions[]`}
+                                            onCheckedChange={(checked) =>
+                                                togglePermission(
+                                                    permission.uuid,
+                                                    checked === true,
+                                                )
+                                            }
                                             value={permission.uuid}
                                         />
                                         <FieldLabel
