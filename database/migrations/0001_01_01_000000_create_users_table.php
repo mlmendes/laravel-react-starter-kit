0c272964bd997c34b904ec1c274b8125e7b15fa3
Lucas Mendes Martins <martinslmendes@gmail.com>
2026-07-16 22:47:32 -0300

    build: unified users table related migration files

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,12 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('welcome_valid_until')->nullable();
             $table->string('password');
+            $table->string('avatar')->nullable();
+            $table->text('two_factor_secret')->nullable();
+            $table->text('two_factor_recovery_codes')->nullable();
+            $table->timestamp('two_factor_confirmed_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
